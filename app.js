721563945ec175312dce78ab9eaf61503a24e86e
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2024-06-14 03:19:09 +0530

    Remove the rate-limiter

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ import { directStream, checkHealth, checkHealthV2 } from "./src/controller.js";
 const app = express();
 const port = process.env.PORT || 3001;
 
-const limiter = rateLimit({
-  windowMs: 4 * 60 * 1000, // 5 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  message: "Stop messing around",
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+// const limiter = rateLimit({
+//   windowMs: 4 * 60 * 1000, // 5 minutes
+//   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//   message: "Stop messing around",
+//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
 
-app.use(limiter);
+// app.use(limiter);
 
 app.get("/", (req, res) => {
   res.send("Server up and running on ");
